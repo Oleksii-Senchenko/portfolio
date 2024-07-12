453d364ed52efd34131e1a68b312cc8f8c7ef69d
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-07-12 17:52:58 +0200

    Generics

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,52 @@
 "use strict";
-const fruits = {
-    apples: 1,
-    orange: 2,
-    banana: 7,
+// type Goods = {
+//   [key: string]: number | string;
+// };
+// const fruits: Goods = {
+//   apples: 1,
+//   orange: 2,
+//   banana: 7,
+// };
+// const vegetables: Goods = {
+//   onion: 2,
+//   cucumber: 32,
+//   potato: "42",
+// };
+/////generics
+///////////////////////////////////////////////////////////
+// function indefy<string>(arg: string): string {        //
+//   return arg;                                         //
+// }                                                     //
+function identity(arg) {
+    return arg; //
+} //
+//
+let output1 = identity('srting'); //
+let output2 = identity(1); //
+///////////////////////////////////////////////////////////
+function reverse(items) {
+    return items.reverse();
+}
+let option1 = reverse([1, 2, 3, 4, 5, 6, 7]);
+let option2 = reverse(['g', 'f', 'e', 'd', 'c', 'b', 'a']);
+// console.log(option1);
+// console.log(option2);
+function lengthOfObject(obj) {
+    return obj.duration;
+}
+lengthOfObject({
+    name: "bob",
+    duration: 12
+});
+////////////////////////////////////
+const students = {
+    name: "Bob",
+    age: 22,
 };
-const vegetables = {
-    onion: 2,
-    cucumber: 32,
-    potato: "42",
-};
+function getProperty(obj, key) {
+    return obj[key];
+}
+let studentName = getProperty(students, "name");
+// let addressName = getProperty(students, "address")
+console.log(studentName);
+// console.log(addressName);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,64 @@
-type Goods = {
-  [key: string]: number | string;
-};
 
-const fruits: Goods = {
-  apples: 1,
-  orange: 2,
-  banana: 7,
-};
+// type Goods = {
+//   [key: string]: number | string;
+// };
 
-const vegetables: Goods = {
-  onion: 2,
-  cucumber: 32,
-  potato: "42",
-};
+// const fruits: Goods = {
+//   apples: 1,
+//   orange: 2,
+//   banana: 7,
+// };
+
+// const vegetables: Goods = {
+//   onion: 2,
+//   cucumber: 32,
+//   potato: "42",
+// };
+
+/////generics
+///////////////////////////////////////////////////////////
+// function indefy<string>(arg: string): string {        //
+//   return arg;                                         //
+// }                                                     //
+function identity<T>(arg: T): T {                        //
+  return arg;                                            //
+}                                                        //
+                                                         //
+let output1 = identity<string>('srting')                 //
+let output2 = identity<number>(1)                        //
+///////////////////////////////////////////////////////////
+
+function reverse<T>(items:T[]):T[] {
+    return items.reverse()
+}
+
+let option1 = reverse<number>([1,2,3,4,5,6,7])
+let option2 = reverse<string>(['g','f','e','d','c','b','a'])
+
+// console.log(option1);
+// console.log(option2);
+
+
+function lengthOfObject<T extends {duration: number}>(obj:T):number {
+    return obj.duration
+}
+
+lengthOfObject({
+    name:"bob",
+    duration: 12
+})
+////////////////////////////////////
+const students ={
+    name: "Bob",
+    age:22,
+}
+
+function getProperty<ObjectType, KeyType extends keyof ObjectType>(obj:ObjectType, key: KeyType) {
+    return obj[key]
+}
+
+let studentName = getProperty(students, "name")
+// let addressName = getProperty(students, "address")
+console.log(studentName);
+// console.log(addressName);
+
