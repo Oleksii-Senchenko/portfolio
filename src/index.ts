9400e1d30a8094deb6ef46a48980b6c75e4b4b18
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-07-12 11:57:13 +0200

    feat: types => any & unknown

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,44 @@
-let total: number = 5;
-let name: string = "Gay";
-let isActive: boolean = true;
+// type User = {
+//     name: string;
+//     age: number;
+//   };
+  
 
-name = "1";
+// let total: number = 5;
+// let name: string = "Gay";
+// let isActive: boolean = true;
 
-const numbers: number[] = [1, 2, 3, 4, 5, 6];
+// name = "1";
+
+// const numbers: number[] = [1, 2, 3, 4, 5, 6];
 
 // const user: { name: string; age: number } = {
 //   name: "Bob",
 //   age: 12,
 // };
 
-type User = {
-  name: string;
-  age: number;
-};
 
-const user: User = {
-  name: "Bob",
-  age: 12,
-};
+// const user: User = {
+//   name: "Bob",
+//   age: 12,
+// };
 
 // error user.age = '12'
 // error user.name = 12
+
+// type eventType = 'lesson' | 'deadline'
+
+// const event: eventType = 'deadline'
+// const event: eventType = 'homeWork'
+
+////////////////Second lesson/////////////////////
+//type any//
+
+// let age: any = 10
+// age = false
+
+//type unkown
+
+let name: unknown = 'Bob'
+
+name = 12
