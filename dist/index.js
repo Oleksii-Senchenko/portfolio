2d28b1b9c16a41ba97a85cc23f97b2694b8a233a
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-07-13 17:34:06 +0200

    feat: Pick, Omit

--- dist/index.js
+++ dist/index.js
@@ -12,23 +12,25 @@
 //   cucumber: 32,
 //   potato: "42",
 // };
-/////generics
+/////GENERICS AND THEIR ABILITIES
 ///////////////////////////////////////////////////////////
 // function indefy<string>(arg: string): string {        //
 //   return arg;                                         //
 // }                                                     //
 function identity(arg) {
+    //
     return arg; //
 } //
 //
-let output1 = identity('srting'); //
+let output1 = identity("srting"); //
 let output2 = identity(1); //
+//
 ///////////////////////////////////////////////////////////
 function reverse(items) {
     return items.reverse();
 }
 let option1 = reverse([1, 2, 3, 4, 5, 6, 7]);
-let option2 = reverse(['g', 'f', 'e', 'd', 'c', 'b', 'a']);
+let option2 = reverse(["g", "f", "e", "d", "c", "b", "a"]);
 // console.log(option1);
 // console.log(option2);
 function lengthOfObject(obj) {
@@ -36,9 +38,10 @@ function lengthOfObject(obj) {
 }
 lengthOfObject({
     name: "bob",
-    duration: 12
+    duration: 12,
 });
 ////////////////////////////////////
+// Here we check if any key belongs to an object element
 const students = {
     name: "Bob",
     age: 22,
@@ -46,7 +49,33 @@ const students = {
 function getProperty(obj, key) {
     return obj[key];
 }
-let studentName = getProperty(students, "name");
-// let addressName = getProperty(students, "address")
-console.log(studentName);
-// console.log(addressName);
+const info = getProperty(students, "age");
+console.log(info);
+const todo1 = {
+    title: "Learn TS",
+    description: "Stydy basick ts",
+    complited: false,
+};
+function updateField(todo, fieldToUpdate) {
+    return { ...todo, ...fieldToUpdate };
+}
+const todo2 = updateField(todo1, { complited: true });
+console.log(todo2);
+const jhon = {
+    name: "Bober",
+    age: 12,
+};
+//user.age = 12 ()=> error becouse we used property "Readonly"
+const arrOfNum = [1, 2, 3, 4, 5, 6, 7];
+const bobSummery = {
+    name: "bob",
+    age: 12
+    // address: "Lavanda" It does not work becouse wo took only name and age in //=>"type PersonSummery"<=\\
+    //IT`S PICK
+};
+const julianSummery = {
+    name: "bob",
+    age: 12,
+    // address: "Lavanda" It does not work becouse wo took all types without address in //=>"type PromisWithOutAddress"<=\\
+    //IT`S OMIT
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 // type Goods = {
 //   [key: string]: number | string;
 // };
@@ -15,50 +14,131 @@
 //   potato: "42",
 // };
 
-/////generics
+/////GENERICS AND THEIR ABILITIES
 ///////////////////////////////////////////////////////////
 // function indefy<string>(arg: string): string {        //
 //   return arg;                                         //
 // }                                                     //
-function identity<T>(arg: T): T {                        //
-  return arg;                                            //
-}                                                        //
-                                                         //
-let output1 = identity<string>('srting')                 //
-let output2 = identity<number>(1)                        //
+function identity<T>(arg: T): T {
+  //
+  return arg; //
+} //
+//
+let output1 = identity<string>("srting"); //
+let output2 = identity<number>(1); //
+//
 ///////////////////////////////////////////////////////////
 
-function reverse<T>(items:T[]):T[] {
-    return items.reverse()
+function reverse<T>(items: T[]): T[] {
+  return items.reverse();
 }
 
-let option1 = reverse<number>([1,2,3,4,5,6,7])
-let option2 = reverse<string>(['g','f','e','d','c','b','a'])
+let option1 = reverse<number>([1, 2, 3, 4, 5, 6, 7]);
+let option2 = reverse<string>(["g", "f", "e", "d", "c", "b", "a"]);
 
 // console.log(option1);
 // console.log(option2);
 
-
-function lengthOfObject<T extends {duration: number}>(obj:T):number {
-    return obj.duration
+function lengthOfObject<T extends { duration: number }>(obj: T): number {
+  return obj.duration;
 }
 
 lengthOfObject({
-    name:"bob",
-    duration: 12
-})
+  name: "bob",
+  duration: 12,
+});
 ////////////////////////////////////
-const students ={
-    name: "Bob",
-    age:22,
+
+// Here we check if any key belongs to an object element
+
+const students = {
+  name: "Bob",
+  age: 22,
+};
+
+function getProperty<ObjectType, KeyType extends keyof ObjectType>(
+  obj: ObjectType,
+  key: KeyType
+) {
+  return obj[key];
 }
 
-function getProperty<ObjectType, KeyType extends keyof ObjectType>(obj:ObjectType, key: KeyType) {
-    return obj[key]
+const info = getProperty(students, "age");
+console.log(info);
+///////////////////////////////////////
+
+// If we want to change the value of any object key, we must use "Partial"
+
+type Todo = {
+  title: string;
+  description: string;
+  complited: boolean;
+};
+
+const todo1: Todo = {
+  title: "Learn TS",
+  description: "Stydy basick ts",
+  complited: false,
+};
+
+function updateField(todo: Todo, fieldToUpdate: Partial<Todo>): Todo {
+  return { ...todo, ...fieldToUpdate };
 }
 
-let studentName = getProperty(students, "name")
-// let addressName = getProperty(students, "address")
-console.log(studentName);
-// console.log(addressName);
+const todo2 = updateField(todo1, { complited: true });
+console.log(todo2);
 
+////////read only Readonly
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const jhon: Readonly<User> = {
+  name: "Bober",
+  age: 12,
+};
+
+//user.age = 12 ()=> error becouse we used property "Readonly"
+
+const arrOfNum: ReadonlyArray<number> = [1, 2, 3, 4, 5, 6, 7];
+
+// arrOfNum[0] = 1 Error becouse we used property "Readonly"
+// arrOfNum.push(6)
+
+
+
+//////////pick
+
+type Person = {
+  age:number,
+  name: string,
+  address: string
+}
+
+type PersonSummery = Pick<Person, 'name' | "age">
+
+const bobSummery: PersonSummery = {
+  name: "bob",
+  age: 12
+  // address: "Lavanda" It does not work becouse wo took only name and age in //=>"type PersonSummery"<=\\
+
+  //IT`S PICK
+}
+
+
+type Perso1 = {
+  age:number,
+  name: string,
+  address: string
+}
+
+type PromisWithOutAddress = Omit<Perso1, 'address'>
+
+const julianSummery: PromisWithOutAddress = {
+  name: "bob",
+  age: 12,
+  // address: "Lavanda" It does not work becouse wo took all types without address in //=>"type PromisWithOutAddress"<=\\
+  //IT`S OMIT
+}
