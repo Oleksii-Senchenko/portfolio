fe539280ada7e6953c51a067c84dd707975f0a5a
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-07-14 21:20:44 +0200

    Feat: any/unknown

--- src/complexTypes.ts
+++ src/complexTypes.ts
@@ -2,10 +2,10 @@
 
 //arr
 const arr: number[] = [2, 3, 4, 5, 6]; //array of numbers (numbers[])
-// arr.push('12')  || error bocouse type !== number
+// arr.push('12')  || error because type !== number
 
 const str: string[] = ["a", "b", "c"];
-// str.push(1) || error bocouse type !== string
+// str.push(1) || error because type !== string
 
 //obj
 
@@ -33,5 +33,19 @@ const user: User = {
 
 type eventType = 'lesson' | 'work'
 
-// const events: eventType = 'close' || error bocouse type !== lesson or work
+// const events: eventType = 'close' || error because type !== lesson or work
 
+
+//any
+
+let age: any = 23
+age = 12
+age = '12'
+
+
+//unknown
+
+let voice: unknown = "high"
+voice = 23
+// voice.toString() || Error because "voice" is unknown what type is in it, so we can't change the "voice"
+//This suggests that the developer will move on, and when finished, should come back and change the property type
