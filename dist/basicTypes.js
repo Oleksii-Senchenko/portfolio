a4093e1cebdcb18c9e52c6130e3271be9ca5b9fa
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-07-14 19:45:53 +0200

    Initual complex types

---
+++ dist/basicTypes.js
@@ -0,0 +1,12 @@
+"use strict";
+//number, string, boolean, null, undefined
+let count = 100; //type number
+let userName = "Bober"; //type string
+let isActive = true; //type boolean
+let empty = null; //type null
+let udf = undefined; //type undefined
+// count = 'qwe' || error bocouse type !== number
+// string = 12 || error bocouse type !== string
+// isActive = 12 || error bocouse type !== boolean
+// empty = "Hey" || error bocouse type !== null
+// udf = null || error bocouse type !== undefined
---
+++ src/basicTypes.ts
@@ -0,0 +1,13 @@
+//number, string, boolean, null, undefined
+
+let count: number = 100; //type number
+let userName: string = "Bober"; //type string
+let isActive: boolean = true; //type boolean
+let empty: null = null; //type null
+let udf: undefined = undefined; //type undefined
+
+// count = 'qwe' || error bocouse type !== number
+// string = 12 || error bocouse type !== string
+// isActive = 12 || error bocouse type !== boolean
+// empty = "Hey" || error bocouse type !== null
+// udf = null || error bocouse type !== undefined
