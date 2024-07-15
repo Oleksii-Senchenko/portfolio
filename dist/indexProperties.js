255dc2494966d4a707834cac19b1e884391b57d3
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-07-15 15:30:51 +0200

    Index properties

---
+++ dist/indexProperties.js
@@ -0,0 +1,11 @@
+"use strict";
+const fruits = {
+    avacado: 32,
+    bananas: 15,
+    blubery: 123,
+};
+const meat = {
+    chiken: 65,
+    rode: 21,
+    iceCream: 23,
+};
---
+++ src/indexProperties.ts
@@ -0,0 +1,15 @@
+type Goods = {
+  [key: string]: number;
+};
+
+const fruits: Goods = {
+  avacado: 32,
+  bananas: 15,
+  blubery: 123,
+};
+
+const meat: Goods = {
+  chiken: 65,
+  rode: 21,
+  iceCream: 23,
+};
